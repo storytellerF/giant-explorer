8df3de91d06d0ed2ace4ac187756f854e0753550
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-01-05 15:28:17 +0800

    给debug下authority增加后缀

--- app/GiantExplorer/giant-explorer/build.gradle.kts
+++ app/GiantExplorer/giant-explorer/build.gradle.kts
@@ -1,4 +1,4 @@
-import com.android.build.api.dsl.ApplicationDefaultConfig
+import com.android.build.api.dsl.VariantDimension
 import com.storyteller_f.version_manager.*
 val versionManager: String by project
 
@@ -25,22 +25,13 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
+baseApp()
+
 android {
 
+    val id = "com.storyteller_f.giant_explorer"
     defaultConfig {
-        applicationId = "com.storyteller_f.giant_explorer"
-    }
-
-    namespace = "com.storyteller_f.giant_explorer"
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-    }
-
-    defaultConfig {
-        registerConfigKey("file-provider")
-        registerConfigKey("file-system-provider")
-        registerConfigKey("file-system-encrypted-provider")
+        applicationId = id
         javaCompileOptions {
             annotationProcessorOptions {
                 compilerArgumentProviders(
@@ -49,6 +40,26 @@ android {
             }
         }
     }
+
+    namespace = "com.storyteller_f.giant_explorer"
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    buildTypes {
+        debug {
+            registerProviderKey("file-provider", id, applicationIdSuffix)
+            registerProviderKey("file-system-provider", id, applicationIdSuffix)
+            registerProviderKey("file-system-encrypted-provider", id, applicationIdSuffix)
+        }
+        release {
+            registerProviderKey("file-provider", id)
+            registerProviderKey("file-system-provider", id)
+            registerProviderKey("file-system-encrypted-provider", id)
+        }
+    }
+
     sourceSets {
         // Adds exported schema location as test app assets.
         getByName("androidTest").assets.srcDir("$projectDir/schemas")
@@ -71,12 +82,12 @@ dependencies {
     implementation("com.j256.simplemagic:simplemagic:1.17")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    implementation("androidx.browser:browser:1.6.0")
-    implementation("androidx.webkit:webkit:1.8.0")
+    implementation("androidx.browser:browser:1.7.0")
+    implementation("androidx.webkit:webkit:1.9.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.window:window:1.2.0-beta04")
+    implementation("androidx.window:window:1.2.0")
 
-    androidTestImplementation("androidx.room:room-testing:2.6.0")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
 
     implementation("com.madgag.spongycastle:core:1.58.0.0")
     implementation("com.madgag.spongycastle:prov:1.58.0.0")
@@ -88,7 +99,6 @@ dependencies {
 }
 constraintCommonUIListVersion(versionManager)
 fileSystemDependency()
-baseApp()
 setupGeneric()
 setupDataBinding()
 setupPreviewFeature()
@@ -124,20 +134,25 @@ fun DependencyHandlerScope.handleSu() {
     implementation("com.github.topjohnwu.libsu:nio:${libsuVersion}")
 }
 
-fun ApplicationDefaultConfig.registerConfigKey(
+/**
+ * 同时设置manifest和buildConfig
+ */
+fun VariantDimension.registerProviderKey(
     identification: String,
+    applicationId: String?,
+    valueSuffix: String? = null
 ) {
     val placeholderKey = placeholderKey(identification)
     val buildConfigKey = buildConfigKey(placeholderKey)
-    val fileSystemProviderEncryptedAuthority = "$applicationId.$identification"
+    val authorityValue = "${applicationId}.$identification${if (valueSuffix == null) "" else "$valueSuffix"}"
 
     // Now we can use ${documentsAuthority} in our Manifest
-    manifestPlaceholders[placeholderKey] = fileSystemProviderEncryptedAuthority
+    manifestPlaceholders[placeholderKey] = authorityValue
     // Now we can use BuildConfig.DOCUMENTS_AUTHORITY in our code
     buildConfigField(
         "String",
         buildConfigKey.toString(),
-        "\"${fileSystemProviderEncryptedAuthority}\""
+        "\"${authorityValue}\""
     )
 }
 
@@ -162,7 +177,9 @@ fun placeholderKey(identification: String): String {
         val c = identification[i++]
         if (c == '-') {
             identifyString.append(identification[i++].uppercase())
-        } else identifyString.append(c)
+        } else {
+            identifyString.append(c)
+        }
     }
     return identifyString.append("Authority").toString()
 }
