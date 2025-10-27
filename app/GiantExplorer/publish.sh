56e39a143383ad110eff6c907d3f201ea2ce06af
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-27 16:26:53 +0800

    fix: permission denied

--- app/GiantExplorer/publish.sh
+++ app/GiantExplorer/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # publish in jitpack
 # group 和版本号使用jitpack 提供的
-./gradlew clean -xtest -xlint giant-explorer-plugin-core:publishToMavenLocal --no-daemon
+sh gradlew clean -xtest -xlint giant-explorer-plugin-core:publishToMavenLocal --no-daemon
